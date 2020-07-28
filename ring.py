13183e6dffd939221acaffc663cd5a01cb05dc2f
unknown <rmramosvii@gmail.com>
2020-07-28 15:41:08 -0700

    Ready for testing, email integration added

--- ring.py
+++ ring.py
@@ -1,20 +1,57 @@
-from ring_doorbell import Ring, Auth
-from oauthlib.oauth2 import MissingTokenError
-from decouple import config
-from pprint import pprint
+from ring_doorbell import Ring, Auth # main Ring API (Unofficial)
+from oauthlib.oauth2 import MissingTokenError # used for authentication 
+from decouple import config # allows for the reading of user and password stored in the .env file
+from time import sleep # used to check for 6-hour checks 
 
+import logging
+import smtplib # API used for sending emails
+
+# debugging logger
+def initialize_logger(): 
+    logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M %p')
+    logging.getLogger().setLevel(logging.INFO)
+
+# asks for the Two-Factor Authentication Code
 def otp_callback():
-    auth_code = input("2FA code: ")
+    auth_code = input("Two-Factor Authentication Code: ")
     return auth_code
 
-def return_battery(ring_obj): 
-    return ring_obj.battery_life
+def send_email(percent):
+    # initialize sender credentials and receiver email
+    sndr_user = config('gmail_user')
+    sndr_pw = config('gmail_pw')
+    rcv_eml = config('gmail_receiver')
+    
+    # creates SMTP session 
+    conn = smtplib.SMTP('smtp.gmail.com', 587) 
+    
+    # start TLS for security 
+    conn.starttls() 
+    
+    # login using the credentials
+    conn.login(sndr_user, sndr_pw) 
+    
+    # email body
+    txt = f'\nThe Front Door Ring Battery is at {percent}"%". Charge it!'
+    subj = "Ring Alert!"
+    message = "From: %s\r\n" % sndr_user + "To: %s\r\n" % rcv_eml + "Subject: %s\r\n" % subj + "\r\n" + txt    
+    
+    # send mail and quit the connection
+    conn.sendmail(sndr_user, rcv_eml, message) 
+    conn.quit() 
 
 def main(): 
+    # initialize logger for debugging
+    initialize_logger()
+
+    # initialize ring account username and password
     username = config('user')
     password = config('pw')
-    auth = Auth("MyProject/1.0", None, None)
 
+    # use the Authenticator of the ring_doorbell API 
+    # tries to fetch token for authentication 
+    # requests user input for the code if necessary
+    auth = Auth("MyProject/1.0", None, None)
     try:
         auth.fetch_token(username, password)
     except MissingTokenError:
@@ -23,9 +60,17 @@ def main():
     ring = Ring(auth)
     ring.update_data()
 
+    # filter the Ring Devices
     devices = ring.devices()
-    pprint(devices)
+    front_door = devices['authorized_doorbots']
 
+    # loop for checking battery life
+    while True:
+        battery_life = front_door[0].battery_life
+        if(battery_life <= 40):
+            send_email(battery_life)
+
+        sleep(21600)
 
 if __name__ == "__main__":
     main()
